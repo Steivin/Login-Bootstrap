bfb56b671fc2eaaa7283f032cc110172d89c6932
Steivin <steivin2000@gmail.com>
2025-06-16 10:35:47 -0500

    Se añadió la pagina principal, con su autenticacion de usuario de archivo js y demás cambios

--- js/Script.js
+++ js/Script.js
@@ -40,12 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
     form.addEventListener('submit', (e) => {
       e.preventDefault(); // evitar que el formulario se envíe y recargue la página
-  
+      
+      
       Swal.fire({
         title: "CAMPOS INCOMPLETOS",
         text: "Completa los campos antes de continuar",
         icon: "error"
-      });
+      }); 
     });
   });
 //console.log(multi);
